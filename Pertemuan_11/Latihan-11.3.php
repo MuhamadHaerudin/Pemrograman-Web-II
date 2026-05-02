86827bca093d7bf85c0c1bbb9278cd62e4852611
MuhamadHaerudin <145971550+MuhamadHaerudin@users.noreply.github.com>
2026-05-02 10:56:57 +0700

    Update Latihan-11.3.php

--- Pertemuan_11/Latihan-11.3.php
+++ Pertemuan_11/Latihan-11.3.php
@@ -1,11 +1,8 @@
 <?php
-// 1. Koneksi ke server MySQL
 $con = mysqli_connect("localhost", "root", "");
 
-// 2. Mengaktifkan database (Wajib menyertakan variabel $con)
 mysqli_select_db($con, "lat_dbase"); 
 
-// 3. Membuat struktur tabel
 $sql = "CREATE TABLE tbl_mhs (
     mhsID int NOT NULL AUTO_INCREMENT,
     PRIMARY KEY(mhsID),
@@ -14,10 +11,8 @@ $sql = "CREATE TABLE tbl_mhs (
     Age int
 )";
 
-// Jalankan query pembuatan tabel
 mysqli_query($con, $sql);
 
-// 4. Input data awal (Gunakan mysqli_query dengan variabel $con)
 $input = mysqli_query($con, "INSERT INTO tbl_mhs(FirstName, LastName, Age) 
           VALUES('Anjar', 'Prabowo', 25)");
 
